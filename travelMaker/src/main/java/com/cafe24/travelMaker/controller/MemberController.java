16c071b09f267eae5c6efee2e78b30b134fca010
129212@naver.com <smart-202-1@smart-202-1-PC>
2020-06-09 10:27:05 +0900

    [travelMaker] 멤버 컨트롤러, 수정, 탈퇴 file : MemberController.java, updateMember.html, deleteMember.html detail : 멤버 컨트롤러 수정, 회원정보 수정 화면, 회원 탈퇴 화면

--- travelMaker/src/main/java/com/cafe24/travelMaker/controller/MemberController.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.cafe24.travelMaker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cafe24.travelMaker.domain.Member;
 
 @Controller
 public class MemberController{
@@ -28,4 +31,38 @@ public class MemberController{
 		
 		return "/member/memberFindPw";
 	}
+	
+
+	@PostMapping("/deleteMember")
+	public String deleteMember(Member member) {
+		System.out.println("<post> deleteMember 컨트롤러 도착~! 장하다 장해 !!");
+		
+		return "Member/myPage";
+	}
+	
+	@GetMapping("/deleteMember")
+	public String deleteMember(Model model) {
+		// System.out.println("<get> deleteMember 컨트롤러 도착~! 장하다 장해 !!");
+		
+		return "Member/deleteMember";
+	}
+	
+	@PostMapping("/updateMember")
+	public String updateMember(Member member) {
+		// System.out.println("<post> updateMember 컨트롤러 도착~! 장하다 장해 !!");
+		
+		return "Member/myPage";
+	}
+	
+	@GetMapping("/updateMember")
+	public String updateMember(Model model) {
+		// System.out.println("<get> updateMember 컨트롤러 도착~! 장하다 장해 !!");
+		
+		return "Member/updateMember";
+	}
+	
+	
+	
+	
+	
 }
--- travelMaker/src/main/java/com/cafe24/travelMaker/domain/Member.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/domain/Member.java
@@ -1,5 +1,5 @@
 package com.cafe24.travelMaker.domain;
 
 public class Member {
-
+	
 }
