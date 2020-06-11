02931d012c6505e338860eeb39d1c24aa9263299
ahkong <ahreum0412@naver.com>
2020-06-11 16:20:40 +0900

    [travelMaker] 리뷰신고처리 미완 푸시용 커밋임

---
+++ travelMaker/src/main/java/com/cafe24/travelMaker/controller/ReportController.java
@@ -0,0 +1,17 @@
+package com.cafe24.travelMaker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReportController {
+	@GetMapping("/manageResReviewReport")
+	public String manageResReviewReport() {
+		return "/report/manageResReviewReport";
+	}
+	
+	@GetMapping("/manageSightsReviewReport")
+	public String manageSightsReviewReport() {
+		return "/report/manageSightsReviewReport";
+	}
+}
