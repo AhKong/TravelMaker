39ca71cca896d101ff65c3d698d8031a67e8f4eb
ahkong <ahreum0412@naver.com>
2020-06-15 16:01:31 +0900

    [TravelMaker] 로그인 / 로그아웃
    file: header.html, JoinLoginController.java,
    JoinLoginServcie.java,login.html, loginJoinLayout.html,
    	Member.java,memberMapper.xml
    detail: 로그인 / 로그아웃  (순수한 로그인,로그아웃 기능만 있음 )

---
+++ travelMaker/src/main/java/com/cafe24/travelMaker/service/JoinLoginService.java
@@ -0,0 +1,25 @@
+package com.cafe24.travelMaker.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cafe24.travelMaker.domain.Member;
+import com.cafe24.travelMaker.mapper.MemberMapper;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+
+public class JoinLoginService {
+public  @Autowired  MemberMapper memberMapper;
+
+public int mIdCheck(String mId) {
+	return memberMapper.mIdCheck(mId);
+}
+
+public Member memberLogin(String mId) {
+	return memberMapper.getMemberInfo(mId);
+}
+
+}
