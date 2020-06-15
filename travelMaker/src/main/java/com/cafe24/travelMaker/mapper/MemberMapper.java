69bb353c23fb4bfc66c1aa083b274f210b561b1b
Administrator <Administrator@MSDN-SPECIAL>
2020-06-15 17:30:09 +0900

    [TravelMaker] 아이디찾기 file: MemberController.java detail: 아이디찾기

--- travelMaker/src/main/java/com/cafe24/travelMaker/mapper/MemberMapper.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/mapper/MemberMapper.java
@@ -10,5 +10,7 @@ public interface MemberMapper {
 
 	public int mIdCheck(String mId);
 	public Member getMemberInfo(String mId);
-
+	
+	public Member findId(String mName, String mTel, String mEmail);
+	
 }
