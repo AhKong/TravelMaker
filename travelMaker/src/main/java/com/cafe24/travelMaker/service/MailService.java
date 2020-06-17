6daa93ffe0a9b2e9add10eea61bf40a38303cb25
ahkong <devahgkong@google.com>
2020-06-17 20:20:35 +0900

    [TravelMaker] 메일 서비스
    file: Mail.java, MailHandler.java,application.properties,MailService.java
    detail: 메일 서비스

--- travelMaker/src/main/java/com/cafe24/travelMaker/service/MailService.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/service/MailService.java
@@ -1,41 +1,35 @@
 package com.cafe24.travelMaker.service;
 
-import java.util.Properties;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+import com.cafe24.travelMaker.config.MailHandler;
+import com.cafe24.travelMaker.domain.Mail;
 
 @Service
 public class MailService {
-	@Autowired public JavaMailSender emailSender;
-	
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	     
-	    mailSender.setUsername("devahkong@gmail.com");
-	    mailSender.setPassword("@a22997307");
-	     
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.debug", "true");
-	     
-	    return mailSender;
-	}
 
-	 public void sendSimpleMessage( String to, String subject, String text) {
-		 		SimpleMailMessage message = new SimpleMailMessage(); 
-		        message.setTo(to); 
-		        message.setSubject(subject); 
-		        message.setText(text);
-		        emailSender.send(message);
-		    }
+	@Autowired private JavaMailSender mailSender;
+    private static final String FROM_ADDRESS = "devahkong@gmail.com";
+
+    public void sendMail(Mail mail) {
+        try {
+            MailHandler mailHandler = new MailHandler(mailSender);
+
+            // 받는 사람
+            mailHandler.setTo(mail.getAddress());
+            // 보내는 사람
+            mailHandler.setFrom(MailService.FROM_ADDRESS);
+            // 제목
+            mailHandler.setSubject(mail.getTitle());
+            // HTML Layout
+           
+            mailHandler.setText(mail.getMessage());
+            
+            mailHandler.send();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
