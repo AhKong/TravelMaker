4686da812942cb687a6f5665ff591af32a441fde
Administrator <Administrator@MSDN-SPECIAL>
2020-06-22 16:58:38 +0900

    [Travelmaker] 아이디 빔밀번호 찾기,통계 index.html, memberFindPw.html,memberFindId.html memberMapper.xml, JoinLoginController.java stats.html detail: 아이디,비번 찾기 파일추가 통계

---
+++ travelMaker/src/main/java/com/cafe24/travelMaker/domain/Stats.java
@@ -0,0 +1,15 @@
+package com.cafe24.travelMaker.domain;
+
+public class Stats {
+	//관광명소 통계 하위3개 별점
+	private String m_gender;
+	private String sig_review;
+	private String sig_like;
+	private String sig_comment;
+	private String sig_traffic;
+	private String sig_satis;
+	private String sig_amenity;
+	//음식점 통계
+	private String res_review;
+	private String res_like;
+}
--- travelMaker/src/main/java/com/cafe24/travelMaker/domain/stats.java
+++
@@ -1,5 +0,0 @@
-package com.cafe24.travelMaker.domain;
-
-public class stats {
-
-}
