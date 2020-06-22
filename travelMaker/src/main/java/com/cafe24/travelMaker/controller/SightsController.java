0e658396a181045843c468469fcabedf8c331ec5
ahkong <ahreum0412@naver.com>
2020-06-22 16:26:20 +0900

    [travelMaker] commit

--- travelMaker/src/main/java/com/cafe24/travelMaker/controller/SightsController.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/controller/SightsController.java
@@ -22,7 +22,6 @@ public class SightsController{
 	@GetMapping("/sightsList")
 	public String sightsList(Model model,@RequestParam(name="search") String search) {
 		System.out.println(search+"<---");
-		System.out.println(sightsService.selectSightsList(search));
 		model.addAttribute("searchWord", search);
 		model.addAttribute("sightsList", sightsService.selectSightsList(search));
 		return "/sights/sightsList";
