b7243fd6c77842bd6c56f6674bb89530d10695f8
rkdalstn8 <rkdalstn8@naver.com>
2020-06-23 17:21:24 +0900

    [travelmaker] 상품,제휴업체

--- travelMaker/src/main/java/com/cafe24/travelMaker/domain/Goods.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/domain/Goods.java
@@ -1,14 +1,19 @@
 package com.cafe24.travelMaker.domain;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class Goods {
 	private String goodsCode;
 	private String memberId;
+	private MultipartFile file = null;
 	private String goodsPhoto;
 	private String goodsCate;
 	private String goodsName;
 	private String goodsPrice;
 	private String goodsDetail;
 	private String goodsRegDate;
+	private String goodsSaleYesNo;
+
 	public String getGoodsCode() {
 		return goodsCode;
 	}
@@ -57,11 +62,27 @@ public class Goods {
 	public void setGoodsRegDate(String goodsRegDate) {
 		this.goodsRegDate = goodsRegDate;
 	}
+	public String getGoodsSaleYesNo() {
+		return goodsSaleYesNo;
+	}
+	public void setGoodsSaleYesNo(String goodsSaleYesNo) {
+		this.goodsSaleYesNo = goodsSaleYesNo;
+	}
+	public MultipartFile getFile() {
+		return file;
+	}
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+	
 	@Override
 	public String toString() {
-		return "Goods [goodsCode=" + goodsCode + ", memberId=" + memberId + ", goodsPhoto=" + goodsPhoto
-				+ ", goodsCate=" + goodsCate + ", goodsName=" + goodsName + ", goodsPrice=" + goodsPrice
-				+ ", goodsDetail=" + goodsDetail + ", goodsRegDate=" + goodsRegDate + "]";
+		return "Goods [goodsCode=" + goodsCode + ", memberId=" + memberId + ", file=" + file + ", goodsPhoto="
+				+ goodsPhoto + ", goodsCate=" + goodsCate + ", goodsName=" + goodsName + ", goodsPrice=" + goodsPrice
+				+ ", goodsDetail=" + goodsDetail + ", goodsRegDate=" + goodsRegDate + ", goodsSaleYesNo="
+				+ goodsSaleYesNo + "]";
 	}
+
+	
 }
 
