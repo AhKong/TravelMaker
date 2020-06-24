6d5f2d8183b076099043257e7e7df76577a447aa
ahkong <ahreum0412@naver.com>
2020-06-24 17:29:33 +0900

    [travelmaker] 로그 시도 하였으나 실패,,

--- travelMaker/src/main/java/com/cafe24/travelMaker/TravelMaker1Application.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/TravelMaker1Application.java
@@ -1,6 +1,8 @@
 package com.cafe24.travelMaker;
 
 import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +12,11 @@ import com.cafe24.travelMaker.service.StorageService;
 @SpringBootApplication
 public class TravelMaker1Application {
 
+	private final static Logger logger =LoggerFactory.getLogger(SpringBootApplication.class);
 	public static void main(String[] args) {
+		logger.info("로거 출력 - {}","값");
+		logger.error("로거 출력 - {}","값");
+		logger.debug("로거 출력 - {}","값");
 		SpringApplication.run(TravelMaker1Application.class, args);
 	}
 	
