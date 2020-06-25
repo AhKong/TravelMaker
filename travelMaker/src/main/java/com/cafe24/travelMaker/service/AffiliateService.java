86ac26790a5d070ce603c97eeff6cddf3021775e
ahkong <ahreum0412@naver.com>
2020-06-25 11:45:57 +0900

    [travelmaker] 제휴업체 회원가입
    files: addAffiliate.html, affiliateMapper.xml, AffiliateService.java,
     	   JoinLoginController.java, JoinLoginService.java
    
    detail : 제휴업체 회원가입 (유효성 검사 전)

--- travelMaker/src/main/java/com/cafe24/travelMaker/service/AffiliateService.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/service/AffiliateService.java
@@ -1,8 +1,16 @@
 package com.cafe24.travelMaker.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cafe24.travelMaker.domain.Affiliate;
+import com.cafe24.travelMaker.mapper.AffiliateMapper;
 
 @Service
 public class AffiliateService {
-
+	@Autowired private AffiliateMapper affiliateMapper;
+	
+	public int addAffiliate(Affiliate aff) {
+		return affiliateMapper.addAffiliate(aff);
+	}
 }
--- travelMaker/src/main/java/com/cafe24/travelMaker/service/JoinLoginService.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/service/JoinLoginService.java
@@ -2,7 +2,9 @@ package com.cafe24.travelMaker.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.cafe24.travelMaker.domain.Affiliate;
 import com.cafe24.travelMaker.domain.Member;
+import com.cafe24.travelMaker.mapper.AffiliateMapper;
 import com.cafe24.travelMaker.mapper.MemberMapper;
 
 import org.springframework.stereotype.Service;
@@ -12,7 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 
 public class JoinLoginService {
-public  @Autowired  MemberMapper memberMapper;
+@Autowired  private MemberMapper memberMapper;
+@Autowired private AffiliateMapper affiliateMapper;
+
+public int addAffiliate(Affiliate aff) {
+	return affiliateMapper.addAffiliate(aff);
+}
 
 public int mIdCheck(String mId) {
 	return memberMapper.mIdCheck(mId);
