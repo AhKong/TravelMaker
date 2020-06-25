2538f5adea583bfd8d423371ac1a85a60f98063a
ahkong <ahreum0412@naver.com>
2020-06-25 12:15:37 +0900

    [TravelMaker] ajax 컨트롤러 추가
    files: AjaxController.java
    detail: 모든 ajax url 은 해당 컨트롤러를 거쳐야함

---
+++ travelMaker/src/main/java/com/cafe24/travelMaker/controller/AjaxController.java
@@ -0,0 +1,11 @@
+package com.cafe24.travelMaker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*ajax 컨트롤러*/
+@Controller
+@RequestMapping("/ajax")
+public class AjaxController {
+	
+}
