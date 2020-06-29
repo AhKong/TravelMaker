c4171cdef022d011c42ac8c11a931691f5fab57b
ahkong <ahreum0412@naver.com>
2020-06-29 09:58:38 +0900

    [TravelMaker] ajax controller 분리

--- travelMaker/src/main/java/com/cafe24/travelMaker/controller/AjaxController.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/controller/AjaxController.java
@@ -28,8 +28,6 @@ import com.cafe24.travelMaker.service.ScrapSightsService;
 @RequestMapping("/ajax")
 public class AjaxController {
 
-	@Autowired private  MailService mailService;
-	@Autowired private  CertSerivce certService;
 	@Autowired private MemberService memberService;
 	@Autowired private  ScrapSightsService scrapsightsservice;
 
@@ -76,38 +74,6 @@ public class AjaxController {
 			return getPw;
 		}
 		
-	@GetMapping("/certEmail")
-	@ResponseBody
-	public HashMap<String,String> certEmail(@RequestParam(name="email" ) String email){
-		/* 이메일 인증 
-		 * 
-		 * */
-		System.out.println("email>>>>"+email);
-		HashMap<String, String> certEmailResult = new HashMap<String,String>();
-		HashMap<String, Object> certEmail = certService.certEmail(email); // 보낼 메일 생성, 랜덤코드 생성 
-		mailService.sendMail((Mail)certEmail.get("mail"));//메일 보내기 
-		certEmailResult.put("randomCode", (String)certEmail.get("randomCode"));
-		return certEmailResult;
-	}
-	
-   /*
-    * 아이디 중복검사
-    * */
-	@PostMapping("/idCheck")
-	public @ResponseBody HashMap<String,String> idCheck(@RequestParam(name="mId") String mId ){
-		System.out.println(mId);
-		
-		HashMap<String, String> idCheckResult = new HashMap<String,String>();
-
-		if(memberService.mIdCheck(mId)==0) {
-			idCheckResult.put("result", "N");
-		} else {
-			idCheckResult.put("result", "Y");
-		}
-		return idCheckResult;
-	}
-	
-
 	// 관광지 스크랩
 	@RequestMapping(value = "/SightsScrap") 
 	@ResponseBody
