10ed12ae3e2e42b054e101af90fd94ba6494e6b1
ahkong <ahreum0412@naver.com>
2020-07-01 12:45:30 +0900

    [TravelMaker] ajax 공통 부분 모듈화
    files: commonAjax.js

---
+++ travelMaker/src/main/resources/static/dist/js/commonAjax.js
@@ -0,0 +1,21 @@
+/**
+ *  ajax 공통 모듈
+ */
+
+var commonAjax = function(url, data, fn, methodType ){
+	if(methodType == undefined){
+		methodType = 'POST';
+	}
+	var request = $.ajax({
+		  url: url,  
+		  method: methodType,
+		  data: data, 
+		  dataType: "json"
+	}); 	     
+
+	request.done(fn);	
+
+	request.fail(function( jqXHR, textStatus ) {
+		  alert( "Request failed: " + textStatus );
+	});
+}
