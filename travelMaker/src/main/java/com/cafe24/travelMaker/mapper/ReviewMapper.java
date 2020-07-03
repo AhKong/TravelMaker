a8022a0920ff3d4f9864bd9284e64f3755def99a
ahkong <ahreum0412@naver.com>
2020-07-03 16:24:51 +0900

    [travelMaker] 평가별 카운팅 수정

--- travelMaker/src/main/java/com/cafe24/travelMaker/mapper/ReviewMapper.java
+++ travelMaker/src/main/java/com/cafe24/travelMaker/mapper/ReviewMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.cafe24.travelMaker.domain.ResReview;
+import com.cafe24.travelMaker.domain.ReviewGrade;
 import com.cafe24.travelMaker.domain.TripType;
 
 @Mapper
@@ -13,7 +14,7 @@ public interface ReviewMapper {
 	public int addResReview(ResReview resReview);
 	public int isWritedReview(String mId, String resNum);
 	public List<ResReview> resReviewList(String resNum);
-	public List<Integer> getGradeCnt(String resNum);
+	public List<ReviewGrade> getGradeCnt(String resNum);
 	
 
 }
