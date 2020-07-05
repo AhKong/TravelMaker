dabaf78c3fc7fa87a8ceec6ce1d2bc0d529f0f0a
ahkong <devahgkong@google.com>
2020-07-05 22:39:40 +0900

    [travelmaker] 리뷰 좋아요 준비중,,

---
+++ travelMaker/src/main/java/com/cafe24/travelMaker/domain/ReviewLike.java
@@ -0,0 +1,50 @@
+package com.cafe24.travelMaker.domain;
+
+public class ReviewLike {
+	private String likeNum;
+	private String mId;
+	private String reviewNum;
+	private String reviewRegDate;
+	private boolean isLiked;
+	
+	public String getLikeNum() {
+		return likeNum;
+	}
+	public void setLikeNum(String likeNum) {
+		this.likeNum = likeNum;
+	}
+	public String getmId() {
+		return mId;
+	}
+	public void setmId(String mId) {
+		this.mId = mId;
+	}
+	public String getReviewNum() {
+		return reviewNum;
+	}
+	public void setReviewNum(String reviewNum) {
+		this.reviewNum = reviewNum;
+	}
+	public String getReviewRegDate() {
+		return reviewRegDate;
+	}
+	public void setReviewRegDate(String reviewRegDate) {
+		this.reviewRegDate = reviewRegDate;
+	}
+	
+	public boolean isLiked() {
+		return isLiked;
+	}
+	public void setLiked(boolean isLiked) {
+		this.isLiked = isLiked;
+	}
+	@Override
+	public String toString() {
+		return "ReviewLike [likeNum=" + likeNum + ", mId=" + mId + ", reviewNum=" + reviewNum + ", reviewRegDate="
+				+ reviewRegDate + ", isLiked=" + isLiked + "]";
+	}
+
+	
+	
+	
+}
