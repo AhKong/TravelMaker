5afa4643a2300b9a26f1a7f01d8047dd84814c8d
ghks941 <wnsrhkghks1@gmail.com>
2020-07-06 11:33:23 +0900

    [travelmaker] 스크랩

--- travelMaker/src/main/resources/static/dist/js/callMyTrip.js
+++ travelMaker/src/main/resources/static/dist/js/callMyTrip.js
@@ -17,7 +17,6 @@
 			});
 
 			request.done(function(data) {
-				alert(JSON.stringify(data));
 				console.log(data.result);
 				var html ='';
 				var myTripList = data.result.length
@@ -36,7 +35,6 @@
 
 				html+= '<div onclick="location.href='+'/myTrip/editTrip'+'"> <span class="glyphicon glyphicon-search" aria-hidden="true"></span><a><strong><i class="fas fa-plus"></i> 내여행 만들기</strong></a></div>'
 			
-				console.log(html)
 				var myTripList = $('#myTripList');
 				myTripList.html(html);
 				$("#scrapTripModal").modal();
@@ -57,21 +55,35 @@
 			
 			var radio = $(this).parents('.modal-content').find('input[name="myTrip"]:checked');
 			var tNum = radio.attr('data-tNum');
+			var indexNum= $('#scrapModal').attr('data-indexnum');
+			console.log(indexNum+'<-----');
+			/*var dIcon = $('.scrapDeleteIcon').index();
+			var sIcon = $('.scrapSelectIcon').index();
+			*/
 			
-			console.log(tNum+"icon");
+			//var indexD = $('.scrapDeleteIcon').index(this);
+			   $('.scrapDeleteIcon').eq(indexNum);
+			   
+			//var indexS = $('.scrapSelectIcon').index(this);
+			   $('.scrapSelectIcon').eq(indexNum);
+			   
+			  var j = $("#scrapModal").index(this); 
+			  console.log(j);
+			  console.log($('#scrapModal').eq(1))
+			  alert($('#scrapModal').index(this));
+
+		   
 			
-		/*	var j = 0;
-			for(j=0; j < 5; j++){
-				j += j
-			}*/
-			
-			if($('#scrapInsertIcon3').css('display') != 'none'){
-				$('#scrapInsertIcon3').hide();
-				$('#scrapDeleteIcon3').show();
-			}else if($('#scrapDeleteIcon3').css('display') != 'none'){
-				$('#scrapInsertIcon3').show();
-				$('#scrapDeleteIcon3').hide();
+			if($('.scrapInsertIcon4').css('display') != 'none'){
+				$('.scrapInsertIcon4').hide();
+				$('.scrapDeleteIcon4').show();
+			}else if($('.scrapDeleteIcon4').css('display') != 'none'){
+				$('.scrapInsertIcon4').show();
+				$('.scrapDeleteIcon4').hide();
 			}
+			
+			
+			
 			 var request = $.ajax({
 				  url: "/ajax/SightsScrap", 
 				  method: "POST",
